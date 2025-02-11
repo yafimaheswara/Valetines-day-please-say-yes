2af735b56d76aa6ab2854135b41c4d4b90d54abe
yafimaheswara <maheswara20072008@gmail.com>
2025-02-11 18:06:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,15 +54,15 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
+    "Kamu Benerann??",
+    "Beneran Gaa??",
+    "Kamu seriuss???",
+    "Please ayoo lahh...",
+    "Pikirkan tentang itu!",
+    "Kalo kqmu pilih no,aku bakal sedih banget...",
+    "Sangat sangat sedih...",
+    "PLEASE BANGETT PLEASE...",
+    "Dahlah. aku capek nanyainnya...",
     "Just kidding, say yes please! ❤️"
 ];
 
